94ac4cf2dbaad91afc9627ff4e1f68335e4f8148
ohseil <sioh0612@gmail.com>
2023-02-03 22:39:07 +0900

    first commit

---
+++ src/main/java/project/CItestProject/CItestProjectApplication.java
@@ -0,0 +1,13 @@
+package project.CItestProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CItestProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CItestProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/project/CItestProject/CItestProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package project.CItestProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CItestProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
