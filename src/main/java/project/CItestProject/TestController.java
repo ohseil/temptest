be057525ca92f3ea51274a1210b83f6206c2facb
ohseil <sioh0612@gmail.com>
2023-02-12 23:11:22 +0900

    for confliction test

--- src/main/java/project/CItestProject/TestController.java
+++ src/main/java/project/CItestProject/TestController.java
@@ -16,6 +16,7 @@ public class TestController {
     @GetMapping
     public ResponseEntity<String> getFunc() {
         String b = a;
+        String d = a;
         return ResponseEntity.ok(a);
     }
 
